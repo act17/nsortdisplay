4aa90e7c96c89a19307a87f672ab4361d4d54593
Aiden Taylor <aiden.taylor@protonmail.com>
2022-08-31 23:45:32 -0600

    Formatted with clang-format.

--- main.c
+++ main.c
@@ -1,37 +1,42 @@
-#include <ncurses.h>
 #include "menu.h"
+#include <ncurses.h>
 
-int main(){
+int main() {
 
-  //NCurses init routine.
+  // NCurses init routine.
   initscr();
   start_color();
   cbreak();
   noecho();
-  
-  //Ensures screen is not too small.
-  int MaxY,MaxX;
-  getmaxyx(stdscr,MaxY,MaxX);
-  if(MaxY < 36 || MaxX < 94){
+
+  // Ensures screen is not too small.
+  int MaxY, MaxX;
+  getmaxyx(stdscr, MaxY, MaxX);
+
+  if (MaxY < 36 || MaxX < 94) {
+
     printf("\n\nError!\nScreen too small!");
-    printf("\nYour Y:	%d\nYour X:	%d",MaxY,MaxX);
+    printf("\nYour Y:	%d\nYour X:	%d", MaxY, MaxX);
     printf("\nMinimum:	36\n		94");
     echo();
     endwin();
     return 1;
   }
 
-  //Macros for configuration.
+  // Macros for configuration.
   menu();
-  int algola = algolsel(-1);		//This macro selects the algorithm.
-  //int algolb = algolsel(algola);	//This is commented, as only doing one algorithm is possible with my current knowledge.
-  int delay = delaysel();		//This macro outputs the delay in miliseconds used in nanosleep();.
-  ncomp(algola,delay);			//This macro does everything relating to the actual comparing.
+  int algola = algolsel(-1); // This macro selects the algorithm.
+  // int algolb = algolsel(algola);	//This is commented, as only doing one
+  // algorithm is possible with my current knowledge.
+  int delay = delaysel(); // This macro outputs the delay in miliseconds used in
+                          // nanosleep();.
+  ncomp(algola,
+        delay); // This macro does everything relating to the actual comparing.
 
-					//It's only going to compare one algorithm, however. It's only being kept in
-					//with all the faculties of two algorithms for the sake of future addition.
+  // It's only going to compare one algorithm, however. It's only being kept in
+  // with all the faculties of two algorithms for the sake of future addition.
 
-  //Ending routine.
+  // Ending routine.
   echo();
   clear();
   endwin();
