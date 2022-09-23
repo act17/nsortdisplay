53a26bc2fdb0508e9d0dd064edf9c23eda44a1b5
Aiden Taylor <aiden.taylor@protonmail.com>
2022-09-22 20:37:25 -0600

    Moved the definition of 'argumentstruct' to this file for convenience.

--- sorts/sorts.h
+++ sorts/sorts.h
@@ -1,7 +1,13 @@
 #ifndef _SORTS_H
 #define _SORTS_H
 
-void* bubblewrap(void * args);
-void* quickwrap(void * args);
+void *bubblewrap(void *args);
+void *quickwrap(void *args);
+
+struct argumentstruct {
+  int delay;
+  int *array;
+  unsigned long *comp;
+};
 
 #endif
