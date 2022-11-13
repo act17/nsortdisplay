0afe6d47f43abbc36906813b69f8f56bffa76d10
Aiden Taylor <aiden@vostro.crap>
2022-11-12 19:41:07 -0700

    Rewritten the entire function as to correct an error that resulted in duplicate array entries.

--- sorts/bubble.c
+++ sorts/bubble.c
@@ -4,42 +4,62 @@
 #include <time.h>
 
 void bubble(int *sorted, int size, int delay, unsigned long *comp) {
+
   // Preparing nanosleep();
   struct timespec ts;
   ts.tv_sec = 0;
   ts.tv_nsec = delay;
 
-  int offset = 0;       // This is an offset that the program uses when scanning
-                        // through entries.
-  double current, next; // This is temporary storage for numbers when swapping.
+  int offset = 0; // This is an offset that the program uses when scanning
+                  // through entries.
 
-  for (int a = 0; a < size;
-       a++) { // This loop repeats until the array is garunteed to be sorted.
+  int current, next; // This is temporary storage for numbers when swapping.
 
-    for (int b = 1; b < size - a; b++) { // This loop repeats until an entry has
-                                         // reached its garunteed proper place.
+  // This loop repeats until the array is garunteed to be sorted.
+  for (int a = 0; a < size; a++) {
+
+    /* This loop runs through each array entry, and swaps them.
+       This repeats until the highest digit (so far) reachest the end
+       of the array. */
+    for (int b = 1; b < size - a; b++) {
 
       current = sorted[offset];
       next = sorted[offset + 1];
 
-      nanosleep(&ts, &ts); // This is to delay the program's comparison.
+      // This routine is performed to pause the comparison for the delay,
+      // and increment the comparison counter by one.
+      nanosleep(&ts, &ts);
       comp[0] = comp[0] + 1;
 
-      if (current > next) { // This occurs when the two entries must be swapped.
+      // This is the comparison. In the case that our current number is
+      // higher...
+      if (current > next) {
+
+        // We define the array entries as their opposite.
         sorted[offset] = next;
-        offset++;
-        sorted[offset] = current;
+        sorted[offset + 1] = current;
+
       }
 
-      else // This occurs when the opposite is true, and the offest - which is
-           // the next highest
-        offset++; // number just by the way - is the next entry in the array.
+      /* If the opposite is true, then no swap needs to be performed.
+         For some reason, this is the determining factor if the sort is
+         completed. Why I need to add this else statement or else the sort
+         duplicates numbers is unknown - but it's been bugging me for months
+         now. */
+      else
+        current = sorted[offset + 1];
+
+      // Regardless, the offset must be then increased by one for the next pass.
+      offset++;
     }
 
-    if (a != (size - 1)) // This is an error prevention measure that ensures our
-                         // final value is what it must be.
+    // Then we need to do a check to ensure that we don't have the special case
+    // of the final value. If so, then we do this guard:
+    if (a != (size - 1))
       sorted[offset] = current;
 
+    // When finished with the current pass, as in, the value reached its correct
+    // place, we then reset the offest.
     offset = 0;
   }
 
